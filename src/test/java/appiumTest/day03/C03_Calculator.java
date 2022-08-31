79c1673ee8d1a1179b96a99b40bd08b7a8f79901
AlkanTuncer <alkantuncer53@hotmail.com>
2022-08-31 22:05:37 +0300

    Appium - Calculator Test with Appium

--- src/test/java/appiumTest/day03/C03_Calculator.java
+++ src/test/java/appiumTest/day03/C03_Calculator.java
@@ -54,4 +54,35 @@ public class C03_Calculator {
         System.out.println("Result = "+result.getText());
 
     }
+
+    @Test
+    public void testCalculator2() throws MalformedURLException{
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel2");
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.0");
+
+        desiredCapabilities.setCapability("appPackage","com.android.calculator2");
+        desiredCapabilities.setCapability("appActivity","com.android.calculator2.Calculator");
+
+
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        WebElement eight = driver.findElement(By.id("com.android.calculator2:id/digit_8"));
+        eight.click();
+
+        WebElement multiply = driver.findElementByAccessibilityId("multiply");
+        multiply.click();
+
+        WebElement seven = driver.findElement(By.id("com.android.calculator2:id/digit_7"));
+        seven.click();
+
+        WebElement equals = driver.findElementByAccessibilityId("equals");
+        equals.click();
+
+        WebElement result = driver.findElement(By.id("com.android.calculator2:id/result"));
+        System.out.println("Result = "+result.getText());
+    }
 }
