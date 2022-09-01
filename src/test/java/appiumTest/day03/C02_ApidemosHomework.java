092802b3d4873dbc557c484318bf71ec45a0302d
AlkanTuncer <alkantuncer53@hotmail.com>
2022-09-01 19:07:21 +0300

    Appium - Apidemos Homework

---
+++ src/test/java/appiumTest/day03/C02_ApidemosHomework.java
@@ -0,0 +1,61 @@
+package appiumTest.day03;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class C02_ApidemosHomework {
+
+    @Test
+    public void testApiDemos() throws MalformedURLException {
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel2");
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.0");
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\alkan\\IdeaProjects\\Appium\\src\\app\\apidemos.apk");
+
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+    /*
+        1. Apidemos'a girilir
+        2. Preference tıklanır
+        3. Preference dependencies tıklanır
+        4. WiFi check box aktif edilir.
+        5. Wifi Settings tıklanır
+        6. Gelen ekrana 'Appium çok güzel' yazılır ve OK tıklanır
+        7. Tekrar tıklayıp yazılan cümleyi dogrulayıp konsola yazdırınız.
+     */
+
+        driver.findElementByAccessibilityId("Preference").click();
+        driver.findElementByAccessibilityId("3. Preference dependencies").click();
+        driver.findElement(By.id("android:id/checkbox")).click();
+        driver.findElement(By.xpath("\t\n" +
+                "/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget." +
+                "LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[2]" +
+                "/android.widget.RelativeLayout")).click();
+        driver.findElement(By.id("android:id/edit")).sendKeys("Appium çok güzel");
+        driver.findElement(By.id("android:id/button1")).click();
+        driver.findElement(By.xpath("\t\n" +
+                "/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget." +
+                "LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[2]" +
+                "/android.widget.RelativeLayout")).click();
+
+        WebElement textBox = driver.findElement(By.id("android:id/edit"));
+        Assert.assertEquals("Appium çok güzel", textBox.getText());
+        System.out.println(textBox.getText());
+
+    }
+}
